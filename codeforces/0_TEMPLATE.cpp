7f2c88e33a1be948c00012d9dc7c75b115e1de14
Bikram Ghuku <bikramghuku05@gmail.com>
2024-09-07 00:14:50 +0530

    feat: added template

---
+++ codeforces/0_TEMPLATE.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+const int M = 1e9 + 7;
+
+void solve(){
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--) solve();
+
+    return 0;
+}

175813174415867fd3baed69293aa14ce191414c
Bikram Ghuku <bikramghuku05@gmail.com>
2024-09-16 20:52:16 +0530

    feat: updated template

--- codeforces/0_TEMPLATE.cpp
+++ codeforces/0_TEMPLATE.cpp
@@ -3,11 +3,17 @@
 using namespace std;
 const int M = 1e9 + 7;
 
+#define FASTIO                        \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);                    \
+	cout.tie(NULL);
+
 void solve(){
     
 }
 
 int main(){
+		FASTIO;
     int t;
     cin>>t;
     while(t--) solve();
